ac1ee1d5607f12aa5e85bbf69257e410e128f7dd
Mike Croft <backtotheoldhouse@gmail.com>
2019-10-07 15:52:06 +0100

    add properties

--- scc-k8s-server/src/main/java/io/mikecroft/scck8sserver/SccK8sServerApplication.java
+++ scc-k8s-server/src/main/java/io/mikecroft/scck8sserver/SccK8sServerApplication.java
@@ -2,7 +2,9 @@ package io.mikecroft.scck8sserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SccK8sServerApplication {
 
